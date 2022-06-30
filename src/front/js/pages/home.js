e55822101ca50db6573674a00fb0c3c6cc6ae8f4
atxoupi <andres.hermelo@gmail.com>
2022-06-30 17:47:35 +0000

    Falta el formulario de registro

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,15 +1,27 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
+import React, {
+    useContext
+} from "react";
+import {
+    Context
+} from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Login } from "./login";
+import {
+    Login
+} from "./login";
 
 export const Home = () => {
-  const { store, actions } = useContext(Context);
+    const {
+        store,
+        actions
+    } = useContext(Context);
+    console.log(store.auth);
 
-  return (
-    <div className="d-flex justify-content-center">
-      <Login />
-    </div>
-  );
+    return ( <
+        div className = "d-flex justify-content-center" >
+        <
+        Login / >
+        <
+        /div>
+    );
 };
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -15,38 +15,41 @@ export const Login = () => {
 
   return (
     <>
-      store.auth === true ? <Navigate to="/demo" />:{" "}
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
+      {store.auth === true ? (
+        <Navigate to="/demo" />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
           </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      )}
     </>
   );
 };
